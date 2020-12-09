36961b4ceb2872d8b27c72b220808aa92dd810ed
Anastasia <Anastasia@DESKTOP-3IO2DP7>
2020-12-09 23:13:58 +0200

    Added common package with Position

---
+++ src/common/Position.java
@@ -0,0 +1,43 @@
+package common;
+
+import jthrow.JThrower;
+
+public class Position 
+{
+	private static final int MIN_VALUE = 0;
+	private int row;
+	private int column;
+	
+	public Position(int row, int column)
+	{
+		this.setRow(row);
+		this.setColumn(column);
+	}
+	
+	public void setRow(int row)
+	{
+		JThrower.throwIf(row, "Row").isLessThan(MIN_VALUE);
+		this.row = row;
+	}
+	
+	public void setColumn(int column)
+	{
+		JThrower.throwIf(column, "Column").isLessThan(MIN_VALUE);
+		this.column = column;
+	}
+	
+	public int getRow()
+	{
+		return this.row;
+	}
+	
+	public int getColumn()
+	{
+		return this.column;
+	}
+	
+	public String toString()
+	{
+		return String.format("Row: %d | Column: %d", this.getRow(), this.getColumn());
+	}
+}
