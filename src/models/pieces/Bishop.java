eb5b99b71203910ad912e5b13935327ac1d60c6f
AnastasiaT0126 <72851610+AnastasiaT0126@users.noreply.github.com>
2020-12-14 22:57:18 +0200

    Bishop Added

--- src/models/pieces/Bishop.java
+++ src/models/pieces/Bishop.java
@@ -1,6 +1,108 @@
 package models.pieces;
 
-public class Bishop 
+import java.util.ArrayList;
+
+import common.Position;
+import enums.PieceColor;
+import enums.PieceType;
+import models.boards.Board;
+
+public class Bishop extends BasePiece
 {
-	//TODO: Implement the Bishop class
+	ArrayList<Position> possibleMoves;
+	private Position newPosition;
+	
+	public Bishop(PieceType type, PieceColor color) {
+		super(type, color);
+		possibleMoves = new ArrayList<>();
+	}
+
+	@Override
+	public ArrayList<Position> getAllReachablePositions(Position currentPosition, Board board) 
+	{		
+		for(int i = currentPosition.getRow(); i < board.getWidth(); i++)
+		{
+			for(int j = currentPosition.getColumn(); j < board.getHeight(); j++)
+			{
+				newPosition.setRow(i + 1);
+				newPosition.setColumn(j + 1);
+				
+				if(!board.isEmptyAt(newPosition))
+				{
+					if(board.getAt(newPosition).getColor() != this.getColor())
+					{
+						possibleMoves.add(newPosition);
+					}
+				}
+				else if(board.isEmptyAt(newPosition))
+				{
+					possibleMoves.add(newPosition);
+				}
+			}
+		}
+		
+		for(int i = currentPosition.getRow(); i >= 0; i--)
+		{
+			for(int j = currentPosition.getColumn(); j >= 0; j--)
+			{
+				newPosition.setRow(i - 1);
+				newPosition.setColumn(j - 1);
+				
+				if(!board.isEmptyAt(newPosition))
+				{
+					if(board.getAt(newPosition).getColor() != this.getColor())
+					{
+						possibleMoves.add(newPosition);
+					}
+				}
+				else if(board.isEmptyAt(newPosition))
+				{
+					possibleMoves.add(newPosition);
+				}
+			}
+		}
+		
+		for(int i = currentPosition.getRow(); i < board.getWidth(); i++)
+		{
+			for(int j = currentPosition.getColumn(); j >= 0; j--)
+			{
+				newPosition.setRow(i + 1);
+				newPosition.setColumn(j - 1);
+				
+				if(!board.isEmptyAt(newPosition))
+				{
+					if(board.getAt(newPosition).getColor() != this.getColor())
+					{
+						possibleMoves.add(newPosition);
+					}
+				}
+				else if(board.isEmptyAt(newPosition))
+				{
+					possibleMoves.add(newPosition);
+				}
+			}
+		}
+		
+		for(int i = currentPosition.getRow(); i >= 0; i--)
+		{
+			for(int j = currentPosition.getColumn(); j < board.getHeight(); j++)
+			{
+				newPosition.setRow(i - 1);
+				newPosition.setColumn(j + 1);
+				
+				if(!board.isEmptyAt(newPosition))
+				{
+					if(board.getAt(newPosition).getColor() != this.getColor())
+					{
+						possibleMoves.add(newPosition);
+					}
+				}
+				else if(board.isEmptyAt(newPosition))
+				{
+					possibleMoves.add(newPosition);
+				}
+			}
+		}
+		return possibleMoves;
+	}
 }
