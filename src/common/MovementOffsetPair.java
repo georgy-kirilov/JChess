91d7f6d0244e606c6e04a4ded61fdda4360dbcc3
dimitar.ivanov <dimitarivanov523@gmail.com>
2020-12-22 20:15:12 +0200

    Make the Knight movement; Make some changes to MovementOffsetPair

--- src/common/MovementOffsetPair.java
+++ src/common/MovementOffsetPair.java
@@ -11,6 +11,16 @@ public class MovementOffsetPair
 	public static final MovementOffsetPair BOTTOM_RIGHT = new MovementOffsetPair(1, 1);
 	public static final MovementOffsetPair BOTTOM_LEFT = new MovementOffsetPair(1, -1);
 	
+	//This is only for the knight don't touch it
+	public static final MovementOffsetPair Knight_2Up_1Left = new MovementOffsetPair(-2, -1);
+	public static final MovementOffsetPair Knight_2Up_1Right = new MovementOffsetPair(-2, 1);
+	public static final MovementOffsetPair Knight_1Up_2Left = new MovementOffsetPair(-1, -2);
+	public static final MovementOffsetPair Knight_1Up_2Right = new MovementOffsetPair(-1, 2);
+	public static final MovementOffsetPair Knight_2Down_1Left = new MovementOffsetPair(2, -1);
+	public static final MovementOffsetPair Knight_2Down_1Right = new MovementOffsetPair(2, 1);
+	public static final MovementOffsetPair Knight_1Down_2Left = new MovementOffsetPair(1, -2);
+	public static final MovementOffsetPair Knight_1Down_2Right = new MovementOffsetPair(1, 2);
+	
 	private int rowOffset;
 	private int columnOffset;
 	
