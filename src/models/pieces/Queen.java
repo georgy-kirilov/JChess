09876d74467aea8579f94a506ffd58e822a001ee
Anastasia <Anastasia@DESKTOP-2TRQPD9>
2020-12-22 11:18:30 +0200

    Added QueenTest and Queen

--- src/models/pieces/Queen.java
+++ src/models/pieces/Queen.java
@@ -1,6 +1,38 @@
 package models.pieces;
 
-public class Queen 
+import java.util.ArrayList;
+
+import common.Position;
+import enums.PieceColor;
+import models.boards.Board;
+
+public class Queen extends BasePiece
 {
-	//TODO: Implement the Queen class
-}
+	public Queen(PieceColor color)
+	{
+		super(color);
+	}
+	
+	@Override
+	public Iterable<Position> getAllReachablePositions(Position currentPosition, Board board) 
+	{
+		ArrayList<Position> allPossiblePositions = new ArrayList<>();
+		//UP
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, -1, 0));
+		//DOWN
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, 1, 0));
+		//RIGHT
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, 0, 1));
+		//LEFT
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, 0, -1));
+		//LEFT DIAGONAL (UP)	
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, -1, -1));
+		//LEFT DIAGONAL (DOWN)
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, 1, -1));
+		//RIGHT DIAGONAL (UP)
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, -1, 1));
+		//RIGHT DIAGONAL (DOWN)
+		allPossiblePositions.addAll(this.getReachableConsequtivePositions(currentPosition, board, 1, 1));
+		return allPossiblePositions;
+		
+	}}
