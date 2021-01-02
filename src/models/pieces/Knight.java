7043ec9ba99659e797b82739a5367f6e1ded6a8e
georgy-kirilov <georgynk17@gmail.com>
2021-01-02 21:13:56 +0200

    Refactored Knight

--- src/models/pieces/Knight.java
+++ src/models/pieces/Knight.java
@@ -6,30 +6,26 @@ import models.boards.Board;
 import common.MovementOffsetPair;
 
 public class Knight extends BasePiece
-{
-	//TODO: Implement the Knight class
-	
+{	
 	public Knight(PieceColor colorOfFigure)
 	{
 		super(colorOfFigure);
 	}
 
 	@Override
-	public Iterable<Position> getAllReachablePositions(Position currentPosition, Board board) {
-		// TODO Auto-generated method stub
-		
+	public Iterable<Position> getAllReachablePositions(Position currentPosition, Board board)
+	{
 		MovementOffsetPair[] offsetPairs = new MovementOffsetPair[]
-				{
-					MovementOffsetPair.Knight_2Up_1Left,
-					MovementOffsetPair.Knight_2Up_1Right,
-					MovementOffsetPair.Knight_1Up_2Left,
-					MovementOffsetPair.Knight_1Up_2Right,
-					MovementOffsetPair.Knight_2Down_1Left,
-					MovementOffsetPair.Knight_2Down_1Right,
-					MovementOffsetPair.Knight_1Down_2Left,
-					MovementOffsetPair.Knight_1Down_2Right
-				};
-		
+		{
+			MovementOffsetPair.Knight_2Up_1Left,
+			MovementOffsetPair.Knight_2Up_1Right,
+			MovementOffsetPair.Knight_1Up_2Left,
+			MovementOffsetPair.Knight_1Up_2Right,
+			MovementOffsetPair.Knight_2Down_1Left,
+			MovementOffsetPair.Knight_2Down_1Right,
+			MovementOffsetPair.Knight_1Down_2Left,
+			MovementOffsetPair.Knight_1Down_2Right,
+		};
 		
 		return this.getReachableSinglePositions(currentPosition, board, offsetPairs);
 	}
