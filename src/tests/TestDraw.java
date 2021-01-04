468e51d9246be36743b8f8a761e9d5dd366356fb
Admin <Admin@DESKTOP-RSJJARR>
2021-01-04 19:22:05 +0200

    Test

--- src/tests/TestDraw.java
+++ src/tests/TestDraw.java
@@ -1,6 +1,7 @@
 package tests;
 
 import java.awt.Font;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 
@@ -9,33 +10,28 @@ import enums.PieceColor;
 import models.boards.Board;
 import models.boards.StandardBoard;
 import models.pieces.Pawn;
-import models.pieces.Queen;
+import models.pieces.Knight;
 import views.gui.CellView;
 
 public class TestDraw {
 
 	public static void main(String[] args) {
-		Board board = new StandardBoard();
-		Queen queen = new Queen(PieceColor.WHITE);	
-		Position position1 = new Position(0, 2);
-		Position position2 = new Position(1, 4);
+		Knight Knight = new Knight(PieceColor.WHITE);
 		Pawn pawn = new Pawn (PieceColor.BLACK);
-		board.setAt(position1, pawn);
-		board.setAt(position2, queen);
 		CellView cv[] = new CellView[2];
-		cv[0] = new CellView(position1.getRow(),position1.getColumn(),pawn);
-		cv[1] = new CellView(position2.getRow(),position2.getColumn(),queen);
+		Rectangle r = new Rectangle(10,20,30,30);
+		cv[0] = new CellView(r,pawn,true);
+		cv[1] = new CellView(r,Knight,false);
 		JFrame  f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setBounds(100,100,900,500);
 		f.setVisible(true);
 		f.setLayout(null);
 		
-		cv[0].setBounds(20,20,300,300);
+		cv[0].setBounds(20,20,80,80);
 		f.add(cv[0]);
-		cv[1].setBounds(320,20,300,300);
+		cv[1].setBounds(270,20,50,50);
 		f.add(cv[1]);
-		
 		
 		System.out.println("ok");
 	}
