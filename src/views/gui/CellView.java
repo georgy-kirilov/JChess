92c6d8cd3b04f939bdc4e666408feeeaceca8fe1
Admin <Admin@DESKTOP-RSJJARR>
2021-01-04 19:21:39 +0200

    implemented cell view

--- src/views/gui/CellView.java
+++ src/views/gui/CellView.java
@@ -7,35 +7,31 @@ import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 import models.pieces.Piece;
 
-public class CellView extends JComponent
+public class CellView extends JPanel
 {
-	
-	private int x;
-	private int y;
-	private static final int WIDTH = 40;
-	private static final int HEIGHT = 40;
 	private Color backround;
-	Piece p;
-	//add piece
+	private Piece p;
 	
 	
 	
-	public CellView(int x,int y, Piece p) 
+	
+	public CellView(Rectangle bounds, Piece p, boolean isCellDark) 
 	{
-		if(x>=0&&x<=7)
-		this.x=x*WIDTH;
-		if(y>=0&&y<=7)		
-		this.y=y*HEIGHT;
-		if(x%2==0&&y%2==0||x%2==1&&y%2==1) {
+		this.setBounds(bounds);
+		
+		
+		if(isCellDark) 
+		{
 			backround = Color.cyan;
 		}
-		else backround = Color.LIGHT_GRAY;	
-		if(p!=null) {
-			this.p=p;
-		}
+		else backround = Color.LIGHT_GRAY;
+		
+		this.p=p;
+		
 	}
 	
 	@Override
@@ -44,15 +40,12 @@ public class CellView extends JComponent
 		super.paintComponent(graphics);
 		Graphics2D g =  (Graphics2D) graphics;
 		
-		g.setColor(Color.white);
-		g.fill(new Rectangle2D.Double(0,0,900,900));
-		g.draw(new Rectangle2D.Double(0,0, 700, 700));
 		
-		g.setColor(backround);
-		g.fill(new Rectangle2D.Double(this.x, this.y, CellView.WIDTH, CellView.HEIGHT));
+		
+		this.setBackground(backround);
 		
 		TextPieceDrawer tpd = new TextPieceDrawer();
-		tpd.drawPiece(g, p, new Rectangle(this.x, this.y, CellView.WIDTH, CellView.HEIGHT));
+		tpd.drawPiece(g, p, this.getBounds());
 	}
 	
 }
