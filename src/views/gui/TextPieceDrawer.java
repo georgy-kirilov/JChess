9795634dbd8fc49d15a1a16c574e85b593a5aa27
Admin <Admin@DESKTOP-RSJJARR>
2021-01-04 19:23:25 +0200

    centered figures

--- src/views/gui/TextPieceDrawer.java
+++ src/views/gui/TextPieceDrawer.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-
+import java.util.HashMap;
 
 import enums.PieceColor;
 import models.pieces.Bishop;
@@ -17,97 +17,43 @@ import models.pieces.Rook;
 
 public class TextPieceDrawer implements PieceDrawer 
 {
-
+	private HashMap<Class<?>, String> piecesAndSymbols = new HashMap<Class<?>, String>();
+	public TextPieceDrawer() 
+	{
+		this.piecesAndSymbols.put(Pawn.class,"♟" );
+		this.piecesAndSymbols.put(Bishop.class, "♝");
+		this.piecesAndSymbols.put(Knight.class, "♞");
+		this.piecesAndSymbols.put(Rook.class, "♜");
+		this.piecesAndSymbols.put(Queen.class, "♛");
+		this.piecesAndSymbols.put(King.class, "♚");
+	}
+	
 	@Override
 	public void drawPiece(Graphics2D g, Piece piece, Rectangle cellDimensions) 
 	{
-		Font s = new Font ("Serif",Font.PLAIN,35);
+		String symbol = "";
+		if(piece != null)
+		{
+			symbol = this.piecesAndSymbols.get(piece.getClass());
+		}
+		Color forGroundColor ;
+		if(piece!=null&&piece.getColor() == PieceColor.WHITE) 
+		{
+			forGroundColor=Color.WHITE;
+		}
+		else
+		{
+			forGroundColor=Color.black;
+		}
+		
+		
+		int size = (int) (cellDimensions.getWidth()/4*3);
+		System.out.println((cellDimensions.getWidth()/2));
+		Font s = new Font ("Serif",Font.PLAIN, size);
+		g.setColor(forGroundColor);
 		g.setFont(s);
-		if(piece.getClass()==Pawn.class) 
-		{
-			if(piece.getColor()==PieceColor.BLACK) 
-			{
-				g.setColor(Color.black);
-				g.drawString("♟", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-			else 
-			{
-				g.setColor(Color.white);
-				g.drawString("♙", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
 		
-			}
-		}
-		else if(piece.getClass()==Bishop.class) 
-		{
-			if(piece.getColor()==PieceColor.BLACK) 
-			{
-				g.setColor(Color.black);
-				g.drawString("♝", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-			else 
-			{
-				g.setColor(Color.white);
-				g.drawString("♗", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-		}
-		else if(piece.getClass()==Knight.class) 
-		{
-			
-			if(piece.getColor()==PieceColor.BLACK) 
-			{
-				g.setColor(Color.black);
-				g.drawString("♞", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-			else 
-			{
-				g.setColor(Color.white);
-				g.drawString("♘", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-		}
-		else if(piece.getClass()==Rook.class) 
-		{
-			
-			if(piece.getColor()==PieceColor.BLACK) 
-			{
-				g.setColor(Color.black);
-				g.drawString("♜", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-			else 
-			{
-				g.setColor(Color.white);
-				g.drawString("♖", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-		}
-		
-		else if(piece.getClass()==Queen.class) 
-		{
-			if(piece.getColor()==PieceColor.BLACK) 
-			{
-				g.setColor(Color.black);
-				g.drawString("♛", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-			else 
-			{
-				g.setColor(Color.white);
-				g.drawString("♕", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-		}
-		
-		else if(piece.getClass()==King.class) 
-		{
-			if(piece.getColor()==PieceColor.BLACK) 
-			{
-				g.setColor(Color.black);
-				g.drawString("♚", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-			else 
-			{
-				g.setColor(Color.white);
-				g.drawString("♔", (int)cellDimensions.getX(), (int)cellDimensions.getY()+35);
-			}
-		}
-		
-		
+		g.drawString(symbol, (int)cellDimensions.getWidth()/2-size/2, (int)cellDimensions.getHeight()-size/3);
 	}
 
 }
