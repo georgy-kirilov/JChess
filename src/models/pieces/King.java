f27f7f1caeb365f9a59c0788c830e60413b7a20a
Dobri <Dobri@DESKTOP-33M0NFR>
2021-01-15 15:56:16 +0200

    Is King in Check update. Checking above,below the king , on the left and on the right for Piece of type Queen or Rook. New push soon

--- src/models/pieces/King.java
+++ src/models/pieces/King.java
@@ -29,4 +29,201 @@ public class King extends BasePiece
 		
 		return this.getReachableSinglePositions(currentPosition, board, offsetPairs);
 	}
+	
+	
+	public boolean isKingInCheck(Position currentPosition, Board board, PieceColor color) {
+		
+		
+		//checking for a Rook or Queen above the King with the opposite of his color
+		for (int i = currentPosition.getRow(); i < 8; i++) {
+			boolean breakOnNonEmpty = false;
+			
+			//if boards position is empty the loop can continue
+			if (board.isEmptyAt(i, currentPosition.getColumn())) {
+				
+				breakOnNonEmpty = false;
+
+				
+			}else{
+				
+				//Checking if there's Rook or Queen on the position
+				if (board.getAt(i, currentPosition.getColumn()).getClass()==Rook.class ||
+						board.getAt(i, currentPosition.getColumn()).getClass()==Queen.class) {
+					
+					//Checking if the color of the Rook/Queen is the opposite of the Kings
+					if ((board.getAt(i, currentPosition.getColumn()).getColor())!=this.getColor()) {
+						
+						
+						//if it's a Rook or a Queen with the opposite color of the King's then we are in Check
+						//returning which means that the King is in Check
+						return true;
+						
+						
+						//if the Piece is not the opposite of the King's color then we are breaking the loop
+					}else{
+						breakOnNonEmpty = true;
+						
+					}
+					
+					
+					// if the Piece is not a Rook or a Queen then we are breaking the loop
+				}else{
+					breakOnNonEmpty = true;
+					
+				}
+				
+				
+				
+				
+			}
+			
+			//If variable breakOnNonEmpty equals true then we break the loop
+			if (breakOnNonEmpty == true) {
+				break;
+			}
+			
+		}
+
+		// checking for a Rook or Queen below the King with the opposite of his color
+		for (int i = currentPosition.getRow(); i>0; i--) {
+			boolean breakOnNonEmpty = false;
+
+			// if boards position is empty the loop can continue
+			if (board.isEmptyAt(i, currentPosition.getColumn())) {
+
+				breakOnNonEmpty = false;
+
+			} else {
+
+				// Checking if there's Rook or Queen on the position
+				if (board.getAt(i, currentPosition.getColumn()).getClass() == Rook.class
+						|| board.getAt(i, currentPosition.getColumn()).getClass() == Queen.class) {
+
+					// Checking if the color of the Rook/Queen is the opposite of the Kings
+					if ((board.getAt(i, currentPosition.getColumn()).getColor()) != this.getColor()) {
+
+						// if it's a Rook or a Queen with the opposite color of the King's then we are
+						// in Check
+						// returning which means that the King is in Check
+						return true;
+
+						// if the Piece is not the opposite of the King's color then we are breaking the
+						// loop
+					} else {
+						breakOnNonEmpty = true;
+
+					}
+
+					// if the Piece is not a Rook or a Queen then we are breaking the loop
+				} else {
+					breakOnNonEmpty = true;
+
+				}
+
+			}
+
+			// If variable breakOnNonEmpty equals true then we break the loop
+			if (breakOnNonEmpty == true) {
+				break;
+			}
+
+		}
+				
+				
+		
+		
+		// checking for a Rook or Queen on the right of the King with the opposite of
+		// his color
+		for (int i = currentPosition.getColumn(); i < 8; i++) {
+			boolean breakOnNonEmpty = false;
+
+			// if boards position is empty the loop can continue
+			if (board.isEmptyAt(currentPosition.getRow(), i)) {
+
+				breakOnNonEmpty = false;
+
+			} else {
+
+				// Checking if there's Rook or Queen on the position
+				if (board.getAt(currentPosition.getRow(), i).getClass() == Rook.class
+						|| board.getAt(currentPosition.getRow(), i).getClass() == Queen.class) {
+
+					// Checking if the color of the Rook/Queen is the opposite of the Kings
+					if ((board.getAt(currentPosition.getRow(), i).getColor()) != this.getColor()) {
+
+						// if it's a Rook or a Queen with the opposite color of the King's then we are
+						// in Check
+						// returning which means that the King is in Check
+						return true;
+
+						// if the Piece is not the opposite of the King's color then we are breaking the
+						// loop
+					} else {
+						breakOnNonEmpty = true;
+
+					}
+
+					// if the Piece is not a Rook or a Queen then we are breaking the loop
+				} else {
+					breakOnNonEmpty = true;
+
+				}
+
+			}
+
+			// If variable breakOnNonEmpty equals true then we break the loop
+			if (breakOnNonEmpty == true) {
+				break;
+			}
+
+		}
+
+		// checking for a Rook or Queen on the left of the King with the opposite of his
+		// color
+		for (int i = currentPosition.getColumn(); i > 0; i--) {
+			boolean breakOnNonEmpty = false;
+
+			// if boards position is empty the loop can continue
+			if (board.isEmptyAt(currentPosition.getRow(), i)) {
+
+				breakOnNonEmpty = false;
+
+			} else {
+
+				// Checking if there's Rook or Queen on the position
+				if (board.getAt(currentPosition.getRow(), i).getClass() == Rook.class
+						|| board.getAt(currentPosition.getRow(), i).getClass() == Queen.class) {
+
+					// Checking if the color of the Rook/Queen is the opposite of the Kings
+					if ((board.getAt(currentPosition.getRow(), i).getColor()) != this.getColor()) {
+
+						// if it's a Rook or a Queen with the opposite color of the King's then we are
+						// in Check
+						// returning which means that the King is in Check
+						return true;
+
+						// if the Piece is not the opposite of the King's color then we are breaking the
+						// loop
+					} else {
+						breakOnNonEmpty = true;
+
+					}
+
+					// if the Piece is not a Rook or a Queen then we are breaking the loop
+				} else {
+					breakOnNonEmpty = true;
+
+				}
+
+			}
+
+			// If variable breakOnNonEmpty equals true then we break the loop
+			if (breakOnNonEmpty == true) {
+				break;
+			}
+
+		}
+				
+		return false;
+	}
 }
