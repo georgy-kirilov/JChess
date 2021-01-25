782ee961e450b41eb6c83b18da7595bb5864ad46
georgy-kirilov <georgynk17@gmail.com>
2021-01-25 12:05:16 +0200

    Refactored Board

--- src/models/boards/StandardBoard.java
+++ src/models/boards/StandardBoard.java
@@ -12,7 +12,7 @@ public class StandardBoard extends BaseBoard
 	public StandardBoard()
 	{
 		super(STANDARD_HEIGHT, STANDARD_WIDTH);
-		this.initializePieces();		
+		initializePieces();		
 	}
 	
 	private void initializePieces()
@@ -25,32 +25,32 @@ public class StandardBoard extends BaseBoard
 			currentPawnPosition.setColumn(i);
 			
 			currentPawnPosition.setRow(blackPawnsRow);
-			this.setAt(currentPawnPosition, new Pawn(PieceColor.BLACK));
+			setAt(currentPawnPosition, new Pawn(PieceColor.BLACK));
 			
 			currentPawnPosition.setRow(whitePawnsRow);
-			this.setAt(currentPawnPosition, new Pawn(PieceColor.WHITE));
+			setAt(currentPawnPosition, new Pawn(PieceColor.WHITE));
 		}
 		
 		int blackKingRow = 0;
 		
-		this.setAt(new Position(blackKingRow, 0), new Rook(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 1), new Knight(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 2), new Bishop(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 3), new Queen(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 4), new King(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 5), new Bishop(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 6), new Knight(PieceColor.BLACK));
-		this.setAt(new Position(blackKingRow, 7), new Rook(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 0), new Rook(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 1), new Knight(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 2), new Bishop(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 3), new Queen(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 4), new King(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 5), new Bishop(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 6), new Knight(PieceColor.BLACK));
+		setAt(new Position(blackKingRow, 7), new Rook(PieceColor.BLACK));
 		
 		int whiteKingRow = 7;
 		
-		this.setAt(new Position(whiteKingRow, 0), new Rook(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 1), new Knight(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 2), new Bishop(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 3), new Queen(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 4), new King(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 5), new Bishop(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 6), new Knight(PieceColor.WHITE));
-		this.setAt(new Position(whiteKingRow, 7), new Rook(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 0), new Rook(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 1), new Knight(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 2), new Bishop(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 3), new Queen(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 4), new King(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 5), new Bishop(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 6), new Knight(PieceColor.WHITE));
+		setAt(new Position(whiteKingRow, 7), new Rook(PieceColor.WHITE));
 	}
 }
