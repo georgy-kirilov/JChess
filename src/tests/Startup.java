7aa2ff06bf291b17baaa21ba3aae722b9a990c28
georgy-kirilov <georgynk17@gmail.com>
2021-01-27 18:33:07 +0200

    Refactored Startup

--- src/tests/Startup.java
+++ src/tests/Startup.java
@@ -4,12 +4,16 @@ import core.GuiChessEngine;
 
 import models.boards.*;
 import views.gui.BoardGuiView;
+import views.gui.drawers.PieceDrawer;
 import views.gui.drawers.TextPieceDrawer;
 
 public class Startup
 {	
 	public static void main(String[] args) 
 	{
-		new GuiChessEngine(new BoardGuiView(new RandomBoard(), new TextPieceDrawer()));
+		PieceDrawer pieceDrawer = new TextPieceDrawer();
+		Board gameboard = new StandardBoard();
+		
+		new GuiChessEngine(new BoardGuiView(gameboard, pieceDrawer));
 	}
 }
